1aeff72660598bb77e087376e39a58801db9d78d
purplegun6 <nekohikaru2@yandex.ru>
2026-07-18 16:44:38 +0700

    fix openrouter

--- services/openai_service.py
+++ services/openai_service.py
@@ -1,19 +1,18 @@
 from openai import OpenAI
 from config import OPENAI_API_KEY
 
-client = OpenAI(api_key=OPENAI_API_KEY)
-
+client = OpenAI(
+    api_key=OPENAI_API_KEY,
+    base_url="https://openrouter.ai/api/v1",
+)
 
 def ask_ai(question: str) -> str:
     response = client.chat.completions.create(
-        model="gpt-4.1-mini",
+        model="openai/gpt-4.1-mini",
         messages=[
             {
                 "role": "system",
-                "content": (
-                    "Ты — дружелюбный эксперт по фильмам. "
-                    "Помогай выбирать фильмы и отвечай кратко и понятно."
-                ),
+                "content": "Ты — дружелюбный эксперт по фильмам. Помогай выбирать фильмы и отвечай кратко и понятно.",
             },
             {
                 "role": "user",
